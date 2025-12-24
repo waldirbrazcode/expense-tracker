5d6dfbb0d9440ae80db6957a0289273413f50fe8
Thiago <waldirbraz99@gmail.com>
2025-12-24 18:17:19 -0300

    Is working

--- cli-expenses.py
+++ cli-expenses.py
@@ -1,5 +1,5 @@
 import argparse
-from expense_tracker import add_expense, delete_expense, summary
+from expense_tracker import add_expense, delete_expense, summary, list_expenses, update_expense
 
 def main():
     parser = argparse.ArgumentParser()
@@ -8,6 +8,8 @@ def main():
     add_parser = subparsers.add_parser('add')
     del_parser = subparsers.add_parser('del')
     summ_parser = subparsers.add_parser('summ')
+    list_parser = subparsers.add_parser('list')
+    update_parser = subparsers.add_parser('upd')
 
     add_parser.add_argument('amount', type=float)
     add_parser.add_argument('desc', type=str)
@@ -16,9 +18,16 @@ def main():
     del_parser.add_argument('id', type=int)
     del_parser.set_defaults(func=delete_expense)
 
-    summ_parser.add_argument('year', type=int)
+    summ_parser.add_argument('-y','--year', type=int)
     summ_parser.add_argument('-m', '--month', type=int)
     summ_parser.set_defaults(func=summary)
+
+    list_parser.set_defaults(func=list_expenses)
+
+    update_parser.add_argument('id', type=int)
+    update_parser.add_argument('-d', '--desc')
+    update_parser.add_argument('-am', '--amount', type=float)
+    update_parser.set_defaults(func=update_expense)
 
     args = parser.parse_args()
 
--- expense_tracker.py
+++ expense_tracker.py
@@ -1,11 +1,107 @@
 import json
 import datetime
+import os
+import random
+from prettytable import PrettyTable
 
 def add_expense(args):
-    print(args.amount, args.desc)
+    amount = args.amount
+    desc = args.desc
+    expenses = load_expenses()
+
+    expense = {
+        'id': len(expenses) + int(10000 * random.random()),
+        'amount': amount,
+        'desc': desc,
+        'creation_date': datetime.datetime.now().strftime('%d/%m/%y'),
+        'month_creation': int(datetime.datetime.now().strftime('%m')),
+        'year_creation': int(datetime.datetime.now().strftime('%Y'))
+    }
+
+    expenses.append(expense)
+
+    with open('expenses.json', 'w') as f:
+        json.dump(expenses, f)
 
 def delete_expense(args):
-    print(args.id)
+    expenses = load_expenses()
+    expense_id = args.id
+
+    for expense in expenses:
+        if expense['id'] == expense_id:
+            expenses.remove(expense)
+    
+    with open('expenses.json', 'w') as f:
+        json.dump(expenses, f)
 
 def summary(args):
-    print(args.year, args.month)
+    expenses = load_expenses()
+    year = args.year
+    month = args.month
+    total = 0
+
+    table = PrettyTable()
+    table.field_names = ['ID', 'Creation Date', 'Description', 'Amount']
+
+    if month == None and year:
+        for expense in expenses:
+            if expense['year_creation'] == year:
+                total += expense['amount']
+                table.add_row([expense['id'], expense['creation_date'], expense['desc'], expense['amount']])
+        print(table)
+        print(f'Total amount for {year}: {total}')
+    elif month and year == None:
+        print('Please include the year')
+    elif month and year:
+        for expense in expenses:
+            if expense['month_creation'] == month and expense['year_creation'] == year:
+                total += expense['amount']
+                table.add_row([expense['id'], expense['creation_date'], expense['desc'], expense['amount']])
+        print(table)
+        print(f'Total amount for {datetime.datetime(1, month, 1).strftime('%B')} of {year}: {total}')
+    elif month == None and year == None:
+        for expense in expenses:
+            total += expense['amount']
+            table.add_row([expense['id'], expense['creation_date'], expense['desc'], expense['amount']])
+        print(table)
+        print('Total of all expenses: ', total)
+
+def list_expenses(args):
+    expenses = load_expenses()
+
+    table = PrettyTable()
+    table.field_names = ['ID', 'Creation Date', 'Description', 'Amount']
+
+    for expense in expenses:
+        table.add_row([expense['id'], expense['creation_date'], expense['desc'], expense['amount']])
+
+    print(table)
+
+def update_expense(args):
+    expenses = load_expenses()
+    new_desc = args.desc
+    new_amount = args.amount
+    expense_id = args.id
+
+    if new_desc and new_amount:
+        for expense in expenses:
+            if expense['id'] == expense_id:
+                expense['desc'] = new_desc
+                expense['amount'] = new_amount
+    elif new_desc == None and new_amount:
+        for expense in expenses:
+            if expense['id'] == expense_id:
+                expense['amount'] = new_amount
+    elif new_desc and new_amount == None:
+        for expense in expenses:
+            if expense['id'] == expense_id:
+                expense['desc'] = new_desc
+    with open('expenses.json', 'w') as f:
+        json.dump(expenses, f)
+
+def load_expenses():
+    if not os.path.exists('expenses.json'):
+        return []
+    
+    with open('expenses.json', 'r') as f:
+        return json.load(f)
